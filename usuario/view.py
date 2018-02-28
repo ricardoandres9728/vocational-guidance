885f885a937369c0af767042e781879e39b7fb11
kmedina93 <kevinmedina_93@hotmail.com>
2018-02-28 18:16:43 -0500

    correccion settings

--- usuario/view.py
+++ usuario/view.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from .model import Usuario, TipoUsuario
 
 usuario_app = Blueprint("usuario", __name__)
+
+
+@usuario_app.route('/usuario/registro')
+def registro_usuario():
+    return render_template('usuario/registro_usuario.html')
