c37b02dbf8a6d416a009abcb16c1ab5406f0b897
ricardoandres97 <ricardoandres9728@hotmail.com>
2019-07-16 15:47:44 -0500

    commit

--- aspirante/model.py
+++ aspirante/model.py
@@ -5,14 +5,7 @@ class Aspirante(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombres = db.Column(db.String(80))
     apellidos = db.Column(db.String(80))
-    id_usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"), unique=True)
+    id_usuario = db.Column(
+        db.Integer, db.ForeignKey("usuario.id"), unique=True)
     newsletter = db.Column(db.Boolean, default=True)
-
-
-class Respuestas(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    id_aspirante = db.Column(db.Integer, db.ForeignKey("aspirante.id"))
-    id_encuesta = db.Column(db.Integer, db.ForeignKey("encuesta.id"))
-    respuestas = db.Column(db.JSON, nullable=False)
     live = db.Column(db.Boolean, default=True)
-
--- encuesta/model.py
+++ encuesta/model.py
@@ -4,6 +4,27 @@ from application import db
 class Encuesta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     id_perfil = db.Column(db.Integer, db.ForeignKey("perfil.id"))
-    preguntas = db.Column(db.JSON)
     live = db.Column(db.Boolean, default=True)
+
+
+class Pregunta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pregunta = db.Column(db.String())
+    live = db.Column(db.Boolean, default=True)
+
+
+class Respuesta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_pregunta = db.Column(db.Integer, db.ForeignKey("pregunta.id"))
+    respuesta = db.Column(db.String())
+    valor = db.Column(db.Integer)
+    live = db.Column(db.Boolean, default=True)
+
+
+class Recomendacion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_pregunta = db.Column(db.Integer, db.ForeignKey("pregunta.id"))
+    respuestas = db.Column(db.JSON, nullable=False)
+    live = db.Column(db.Boolean, default=True)
+
 
